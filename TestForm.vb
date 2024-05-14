53a8e8b01d3c14fa1eadef6e15d0d784e83f2d7b
SaniaFarhan <163200670+SaniaFarhan@users.noreply.github.com>
2024-05-14 13:27:58 +0500

    Update TestForm.vb again
    chkbox 2 and chkbox 3 #1 
    
    Signed-off-by: SaniaFarhan <163200670+SaniaFarhan@users.noreply.github.com>

--- TestForm.vb
+++ TestForm.vb
@@ -2,4 +2,12 @@
 Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
               MessageBox.Show("Hello World")
 End Sub
+
+  Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+              MessageBox.Show("Hello")
+End Sub
+
+  Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+              MessageBox.Show("World")
+End Sub
 End Class
