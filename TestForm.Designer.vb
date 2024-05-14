f04fde79b7abb70f413dfdb24bd3c0e94b9c004d
S Farhan <s.farhan81719@gmail.com>
2024-05-14 13:05:10 +0500

    New test form, add checkboxes #2

--- TestForm.Designer.vb
+++ TestForm.Designer.vb
@@ -22,10 +22,84 @@ Partial Class TestForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        GroupBox1 = New GroupBox()
+        CheckBox1 = New CheckBox()
+        CheckBox2 = New CheckBox()
+        CheckBox3 = New CheckBox()
+        CheckBox4 = New CheckBox()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(CheckBox4)
+        GroupBox1.Controls.Add(CheckBox3)
+        GroupBox1.Controls.Add(CheckBox2)
+        GroupBox1.Controls.Add(CheckBox1)
+        GroupBox1.Location = New Point(12, 12)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(140, 239)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "GroupBox1"
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(11, 50)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(85, 19)
+        CheckBox1.TabIndex = 0
+        CheckBox1.Text = "CheckBox1"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(11, 93)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(85, 19)
+        CheckBox2.TabIndex = 1
+        CheckBox2.Text = "CheckBox2"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox3
+        ' 
+        CheckBox3.AutoSize = True
+        CheckBox3.Location = New Point(11, 136)
+        CheckBox3.Name = "CheckBox3"
+        CheckBox3.Size = New Size(85, 19)
+        CheckBox3.TabIndex = 2
+        CheckBox3.Text = "CheckBox3"
+        CheckBox3.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox4
+        ' 
+        CheckBox4.AutoSize = True
+        CheckBox4.Location = New Point(11, 178)
+        CheckBox4.Name = "CheckBox4"
+        CheckBox4.Size = New Size(85, 19)
+        CheckBox4.TabIndex = 3
+        CheckBox4.Text = "CheckBox4"
+        CheckBox4.UseVisualStyleBackColor = True
+        ' 
+        ' TestForm
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(GroupBox1)
+        Name = "TestForm"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
     End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
 
 End Class
