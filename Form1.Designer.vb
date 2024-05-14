9dfb497e35beb9fb99588fc63a86d4aef3ca39ef
S Farhan <s.farhan81719@gmail.com>
2024-05-14 12:51:55 +0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,483 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnSubmit = New Button()
+        lblFName = New Label()
+        txtFName = New TextBox()
+        txtLName = New TextBox()
+        lblLName = New Label()
+        txtAddress = New TextBox()
+        lblAddress = New Label()
+        lblGender = New Label()
+        rdbtnMale = New RadioButton()
+        rdbtnFemale = New RadioButton()
+        date_birth = New DateTimePicker()
+        lblDoB = New Label()
+        txtPass = New TextBox()
+        lblPass = New Label()
+        txtUser = New TextBox()
+        lblUser = New Label()
+        PictureBox1 = New PictureBox()
+        Label1 = New Label()
+        OpenFileDialog1 = New OpenFileDialog()
+        txtImage = New TextBox()
+        Label2 = New Label()
+        chkShowPass = New CheckBox()
+        btnImage = New Button()
+        txtFull = New TextBox()
+        cmbFname = New ComboBox()
+        cmbLname = New ComboBox()
+        cmbAddress = New ComboBox()
+        cmbUser = New ComboBox()
+        cmbPass = New ComboBox()
+        cmbPhoto = New ComboBox()
+        FlowLayoutPanel1 = New FlowLayoutPanel()
+        btnUpdate = New Button()
+        btnDelete = New Button()
+        btnReset = New Button()
+        DataGrid1 = New DataGridView()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        FlowLayoutPanel1.SuspendLayout()
+        CType(DataGrid1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' btnSubmit
+        ' 
+        btnSubmit.Location = New Point(3, 3)
+        btnSubmit.Name = "btnSubmit"
+        btnSubmit.Size = New Size(75, 23)
+        btnSubmit.TabIndex = 0
+        btnSubmit.Text = "Insert"
+        btnSubmit.UseVisualStyleBackColor = True
+        ' 
+        ' lblFName
+        ' 
+        lblFName.Font = New Font("Tahoma", 9.75F)
+        lblFName.Location = New Point(37, 167)
+        lblFName.Name = "lblFName"
+        lblFName.Size = New Size(150, 23)
+        lblFName.TabIndex = 1
+        lblFName.Text = "First Name*"
+        lblFName.TextAlign = ContentAlignment.BottomLeft
+        ' 
+        ' txtFName
+        ' 
+        txtFName.Font = New Font("Tahoma", 9.75F)
+        txtFName.Location = New Point(191, 167)
+        txtFName.Name = "txtFName"
+        txtFName.Size = New Size(285, 23)
+        txtFName.TabIndex = 2
+        ' 
+        ' txtLName
+        ' 
+        txtLName.Font = New Font("Tahoma", 9.75F)
+        txtLName.Location = New Point(191, 196)
+        txtLName.Name = "txtLName"
+        txtLName.Size = New Size(285, 23)
+        txtLName.TabIndex = 4
+        ' 
+        ' lblLName
+        ' 
+        lblLName.Font = New Font("Tahoma", 9.75F)
+        lblLName.Location = New Point(37, 193)
+        lblLName.Name = "lblLName"
+        lblLName.Size = New Size(150, 23)
+        lblLName.TabIndex = 3
+        lblLName.Text = "Last Name*"
+        lblLName.TextAlign = ContentAlignment.BottomLeft
+        ' 
+        ' txtAddress
+        ' 
+        txtAddress.Font = New Font("Tahoma", 9.75F)
+        txtAddress.Location = New Point(191, 225)
+        txtAddress.Name = "txtAddress"
+        txtAddress.Size = New Size(285, 23)
+        txtAddress.TabIndex = 6
+        ' 
+        ' lblAddress
+        ' 
+        lblAddress.Font = New Font("Tahoma", 9.75F)
+        lblAddress.Location = New Point(37, 222)
+        lblAddress.Name = "lblAddress"
+        lblAddress.Size = New Size(150, 23)
+        lblAddress.TabIndex = 5
+        lblAddress.Text = "Address"
+        lblAddress.TextAlign = ContentAlignment.BottomLeft
+        ' 
+        ' lblGender
+        ' 
+        lblGender.Font = New Font("Tahoma", 9.75F)
+        lblGender.Location = New Point(37, 255)
+        lblGender.Name = "lblGender"
+        lblGender.Size = New Size(150, 23)
+        lblGender.TabIndex = 7
+        lblGender.Text = "Gender*"
+        lblGender.TextAlign = ContentAlignment.BottomLeft
+        ' 
+        ' rdbtnMale
+        ' 
+        rdbtnMale.AutoSize = True
+        rdbtnMale.Font = New Font("Tahoma", 9.75F)
+        rdbtnMale.Location = New Point(208, 259)
+        rdbtnMale.Name = "rdbtnMale"
+        rdbtnMale.Size = New Size(52, 20)
+        rdbtnMale.TabIndex = 8
+        rdbtnMale.Text = "Male"
+        rdbtnMale.UseVisualStyleBackColor = True
+        ' 
+        ' rdbtnFemale
+        ' 
+        rdbtnFemale.AutoSize = True
+        rdbtnFemale.Font = New Font("Tahoma", 9.75F)
+        rdbtnFemale.Location = New Point(331, 258)
+        rdbtnFemale.Name = "rdbtnFemale"
+        rdbtnFemale.Size = New Size(67, 20)
+        rdbtnFemale.TabIndex = 9
+        rdbtnFemale.Text = "Female"
+        rdbtnFemale.UseVisualStyleBackColor = True
+        ' 
+        ' date_birth
+        ' 
+        date_birth.CalendarFont = New Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        date_birth.CustomFormat = "yyyy-MM-dd"
+        date_birth.Format = DateTimePickerFormat.Custom
+        date_birth.Location = New Point(191, 288)
+        date_birth.Name = "date_birth"
+        date_birth.Size = New Size(285, 23)
+        date_birth.TabIndex = 10
+        ' 
+        ' lblDoB
+        ' 
+        lblDoB.Font = New Font("Tahoma", 9.75F)
+        lblDoB.Location = New Point(37, 287)
+        lblDoB.Name = "lblDoB"
+        lblDoB.Size = New Size(150, 23)
+        lblDoB.TabIndex = 11
+        lblDoB.Text = "Date of Birth"
+        lblDoB.TextAlign = ContentAlignment.BottomLeft
+        ' 
+        ' txtPass
+        ' 
+        txtPass.Font = New Font("Tahoma", 9.75F)
+        txtPass.Location = New Point(191, 106)
+        txtPass.Name = "txtPass"
+        txtPass.Size = New Size(285, 23)
+        txtPass.TabIndex = 15
+        txtPass.UseSystemPasswordChar = True
+        ' 
+        ' lblPass
+        ' 
+        lblPass.Font = New Font("Tahoma", 9.75F)
+        lblPass.Location = New Point(37, 106)
+        lblPass.Name = "lblPass"
+        lblPass.Size = New Size(150, 23)
+        lblPass.TabIndex = 14
+        lblPass.Text = "Password*"
+        lblPass.TextAlign = ContentAlignment.BottomLeft
+        ' 
+        ' txtUser
+        ' 
+        txtUser.Font = New Font("Tahoma", 9.75F)
+        txtUser.Location = New Point(191, 77)
+        txtUser.Name = "txtUser"
+        txtUser.Size = New Size(285, 23)
+        txtUser.TabIndex = 13
+        ' 
+        ' lblUser
+        ' 
+        lblUser.Font = New Font("Tahoma", 9.75F)
+        lblUser.Location = New Point(37, 77)
+        lblUser.Name = "lblUser"
+        lblUser.Size = New Size(150, 23)
+        lblUser.TabIndex = 12
+        lblUser.Text = "Username*"
+        lblUser.TextAlign = ContentAlignment.BottomLeft
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BorderStyle = BorderStyle.FixedSingle
+        PictureBox1.Location = New Point(530, 52)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(98, 104)
+        PictureBox1.TabIndex = 16
+        PictureBox1.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Tahoma", 24F)
+        Label1.Location = New Point(255, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(176, 39)
+        Label1.TabIndex = 17
+        Label1.Text = "Login Form"
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.FileName = "OpenFileDialog1"
+        OpenFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All files (*.*)|*.*"
+        ' 
+        ' txtImage
+        ' 
+        txtImage.Font = New Font("Tahoma", 9.75F)
+        txtImage.Location = New Point(191, 316)
+        txtImage.Name = "txtImage"
+        txtImage.Size = New Size(285, 23)
+        txtImage.TabIndex = 19
+        ' 
+        ' Label2
+        ' 
+        Label2.Font = New Font("Tahoma", 9.75F)
+        Label2.Location = New Point(39, 316)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(150, 23)
+        Label2.TabIndex = 18
+        Label2.Text = "Photo"
+        Label2.TextAlign = ContentAlignment.BottomLeft
+        ' 
+        ' chkShowPass
+        ' 
+        chkShowPass.AutoSize = True
+        chkShowPass.Location = New Point(195, 140)
+        chkShowPass.Name = "chkShowPass"
+        chkShowPass.Size = New Size(108, 19)
+        chkShowPass.TabIndex = 20
+        chkShowPass.Text = "Show Password"
+        chkShowPass.UseVisualStyleBackColor = True
+        ' 
+        ' btnImage
+        ' 
+        btnImage.AutoSize = True
+        btnImage.Location = New Point(195, 345)
+        btnImage.Name = "btnImage"
+        btnImage.Size = New Size(105, 25)
+        btnImage.TabIndex = 21
+        btnImage.Text = "Select Image File"
+        btnImage.UseVisualStyleBackColor = True
+        ' 
+        ' txtFull
+        ' 
+        txtFull.Location = New Point(390, 723)
+        txtFull.Name = "txtFull"
+        txtFull.Size = New Size(285, 23)
+        txtFull.TabIndex = 22
+        txtFull.Visible = False
+        ' 
+        ' cmbFname
+        ' 
+        cmbFname.Font = New Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        cmbFname.FormattingEnabled = True
+        cmbFname.Location = New Point(99, 662)
+        cmbFname.Name = "cmbFname"
+        cmbFname.Size = New Size(285, 22)
+        cmbFname.TabIndex = 23
+        cmbFname.Text = "Select First Name"
+        cmbFname.Visible = False
+        ' 
+        ' cmbLname
+        ' 
+        cmbLname.Font = New Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        cmbLname.FormattingEnabled = True
+        cmbLname.Location = New Point(99, 694)
+        cmbLname.Name = "cmbLname"
+        cmbLname.Size = New Size(285, 22)
+        cmbLname.TabIndex = 24
+        cmbLname.Text = "Select Last Name"
+        cmbLname.Visible = False
+        ' 
+        ' cmbAddress
+        ' 
+        cmbAddress.Font = New Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        cmbAddress.FormattingEnabled = True
+        cmbAddress.Location = New Point(99, 724)
+        cmbAddress.Name = "cmbAddress"
+        cmbAddress.Size = New Size(285, 22)
+        cmbAddress.TabIndex = 25
+        cmbAddress.Text = "Select Address"
+        cmbAddress.Visible = False
+        ' 
+        ' cmbUser
+        ' 
+        cmbUser.Font = New Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        cmbUser.FormattingEnabled = True
+        cmbUser.Location = New Point(191, 52)
+        cmbUser.Name = "cmbUser"
+        cmbUser.Size = New Size(285, 22)
+        cmbUser.TabIndex = 28
+        cmbUser.Text = "Select username"
+        ' 
+        ' cmbPass
+        ' 
+        cmbPass.Font = New Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        cmbPass.FormattingEnabled = True
+        cmbPass.Location = New Point(390, 664)
+        cmbPass.Name = "cmbPass"
+        cmbPass.Size = New Size(285, 22)
+        cmbPass.TabIndex = 29
+        cmbPass.Text = "Select password"
+        cmbPass.Visible = False
+        ' 
+        ' cmbPhoto
+        ' 
+        cmbPhoto.Font = New Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        cmbPhoto.FormattingEnabled = True
+        cmbPhoto.Location = New Point(390, 694)
+        cmbPhoto.Name = "cmbPhoto"
+        cmbPhoto.Size = New Size(285, 22)
+        cmbPhoto.TabIndex = 30
+        cmbPhoto.Text = "Select file path"
+        cmbPhoto.Visible = False
+        ' 
+        ' FlowLayoutPanel1
+        ' 
+        FlowLayoutPanel1.Controls.Add(btnSubmit)
+        FlowLayoutPanel1.Controls.Add(btnUpdate)
+        FlowLayoutPanel1.Controls.Add(btnDelete)
+        FlowLayoutPanel1.Controls.Add(btnReset)
+        FlowLayoutPanel1.Location = New Point(153, 393)
+        FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        FlowLayoutPanel1.Size = New Size(359, 37)
+        FlowLayoutPanel1.TabIndex = 33
+        ' 
+        ' btnUpdate
+        ' 
+        btnUpdate.Location = New Point(84, 3)
+        btnUpdate.Name = "btnUpdate"
+        btnUpdate.Size = New Size(75, 23)
+        btnUpdate.TabIndex = 2
+        btnUpdate.Text = "Update"
+        btnUpdate.UseVisualStyleBackColor = True
+        ' 
+        ' btnDelete
+        ' 
+        btnDelete.Location = New Point(165, 3)
+        btnDelete.Name = "btnDelete"
+        btnDelete.Size = New Size(75, 23)
+        btnDelete.TabIndex = 1
+        btnDelete.Text = "Delete"
+        btnDelete.UseVisualStyleBackColor = True
+        ' 
+        ' btnReset
+        ' 
+        btnReset.AutoSize = True
+        btnReset.Location = New Point(246, 3)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(109, 25)
+        btnReset.TabIndex = 0
+        btnReset.Text = "Reset"
+        btnReset.UseVisualStyleBackColor = True
+        ' 
+        ' DataGrid1
+        ' 
+        DataGrid1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGrid1.Location = New Point(39, 447)
+        DataGrid1.Name = "DataGrid1"
+        DataGrid1.Size = New Size(601, 209)
+        DataGrid1.TabIndex = 3
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(677, 749)
+        Controls.Add(FlowLayoutPanel1)
+        Controls.Add(cmbPhoto)
+        Controls.Add(cmbPass)
+        Controls.Add(cmbUser)
+        Controls.Add(DataGrid1)
+        Controls.Add(cmbAddress)
+        Controls.Add(cmbLname)
+        Controls.Add(cmbFname)
+        Controls.Add(txtFull)
+        Controls.Add(btnImage)
+        Controls.Add(chkShowPass)
+        Controls.Add(txtImage)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox1)
+        Controls.Add(txtPass)
+        Controls.Add(lblPass)
+        Controls.Add(txtUser)
+        Controls.Add(lblUser)
+        Controls.Add(lblDoB)
+        Controls.Add(date_birth)
+        Controls.Add(rdbtnFemale)
+        Controls.Add(rdbtnMale)
+        Controls.Add(lblGender)
+        Controls.Add(txtAddress)
+        Controls.Add(lblAddress)
+        Controls.Add(txtLName)
+        Controls.Add(lblLName)
+        Controls.Add(txtFName)
+        Controls.Add(lblFName)
+        FormBorderStyle = FormBorderStyle.FixedDialog
+        MaximizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Login Form"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        FlowLayoutPanel1.ResumeLayout(False)
+        FlowLayoutPanel1.PerformLayout()
+        CType(DataGrid1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents lblFName As Label
+    Friend WithEvents txtFName As TextBox
+    Friend WithEvents txtLName As TextBox
+    Friend WithEvents lblLName As Label
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents lblAddress As Label
+    Friend WithEvents lblGender As Label
+    Friend WithEvents rdbtnMale As RadioButton
+    Friend WithEvents rdbtnFemale As RadioButton
+    Friend WithEvents date_birth As DateTimePicker
+    Friend WithEvents lblDoB As Label
+    Friend WithEvents txtPass As TextBox
+    Friend WithEvents lblPass As Label
+    Friend WithEvents txtUser As TextBox
+    Friend WithEvents lblUser As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents txtImage As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents chkShowPass As CheckBox
+    Friend WithEvents btnImage As Button
+    Friend WithEvents txtFull As TextBox
+    Friend WithEvents cmbFname As ComboBox
+    Friend WithEvents cmbLname As ComboBox
+    Friend WithEvents cmbAddress As ComboBox
+    Friend WithEvents cmbUser As ComboBox
+    Friend WithEvents cmbPass As ComboBox
+    Friend WithEvents cmbPhoto As ComboBox
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents DataGrid1 As DataGridView
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,248 @@
+﻿Imports System.Data.Common
+Imports System.Data.SqlClient
+Imports System.IO
+Imports Microsoft.Data.SqlClient
+
+Public Class Form1
+    Inherits System.Windows.Forms.Form
+    Private myConn As SqlConnection
+    Private myCmd As SqlCommand
+    Private mybindingSource As New BindingSource
+    Private dataAdapter As New SqlDataAdapter()
+
+
+    Private myReader As SqlDataReader
+    Private connectionString As String = "Data Source=.;Initial Catalog=TestDB;Integrated Security=SSPI;"
+
+    ' -------- ACTION ON FORM LOAD : COMBOBOX AND GRID FILL-----
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            FillComboBox()
+            ShowGrid()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    ' -------- DATA GRID FILL RECORDS FROM SQL TABLE -----
+
+    Private Sub ShowGrid()
+        Using myConn As New SqlConnection(connectionString)
+            Dim query As String = "SELECT * FROM table1"
+            Dim dataAdapter As New SqlDataAdapter(query, myConn)
+            Dim dataTable As New DataTable
+            dataAdapter.Fill(dataTable)
+            DataGrid1.DataSource = dataTable
+        End Using
+    End Sub
+
+    ' -------- COMBO BOX : ADD USERNAMES FROM SQL TABLE -----
+    Private Sub FillComboBox()
+        Using myConn As New SqlConnection(connectionString)
+            Dim query As String = "SELECT fname, lname, address, username, password, photo FROM table1"
+            Dim cmd As New SqlCommand(query, myConn)
+            myConn.Open()
+            Dim reader As SqlDataReader = cmd.ExecuteReader()
+            While reader.Read()
+                cmbFname.Items.Add(reader("fname").ToString())
+                cmbLname.Items.Add(reader("lname").ToString())
+                cmbAddress.Items.Add(reader("address").ToString())
+                cmbUser.Items.Add(reader("username").ToString())
+                cmbPass.Items.Add(reader("password").ToString())
+                cmbPhoto.Items.Add(reader("photo").ToString())
+            End While
+            reader.Close()
+        End Using
+    End Sub
+
+
+    ' -------- SUBMIT BUTTOM : SUBMIT DATA RECORD INTO SQL TABLE -----
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        If String.IsNullOrEmpty(txtFName.Text) OrElse String.IsNullOrEmpty(txtLName.Text) OrElse
+            String.IsNullOrEmpty(txtUser.Text) OrElse String.IsNullOrEmpty(txtPass.Text) OrElse
+            (Not rdbtnMale.Checked AndAlso Not rdbtnFemale.Checked) Then
+            MessageBox.Show("Please fill all required fields", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        Try
+            Using myConn As New SqlConnection(connectionString)
+                Dim query As String = "SELECT username FROM table1 WHERE username=@username"
+                Dim cmd As New SqlCommand(query, myConn)
+                cmd.Parameters.AddWithValue("@username", txtUser.Text)
+                myConn.Open()
+                Dim existingUsername As Object = cmd.ExecuteScalar()
+                If existingUsername IsNot Nothing Then
+                    MessageBox.Show("Username already exists. Please choose a different username.", "Error")
+                    Return
+                End If
+
+                query = "INSERT INTO table1 (fname, lname, address, gender, DoB, username, password, photo) " &
+                        "VALUES (@fname, @lname, @address, @gender, @DoB, @username, @password, @photo)"
+                cmd.CommandText = query
+                cmd.Parameters.Clear()
+                cmd.Parameters.AddWithValue("@fname", txtFName.Text)
+                cmd.Parameters.AddWithValue("@lname", txtLName.Text)
+                cmd.Parameters.AddWithValue("@address", txtAddress.Text)
+                cmd.Parameters.AddWithValue("@gender", If(rdbtnMale.Checked, 1, 0))
+                cmd.Parameters.AddWithValue("@DoB", date_birth.Value)
+                cmd.Parameters.AddWithValue("@username", txtUser.Text)
+                cmd.Parameters.AddWithValue("@password", txtPass.Text)
+                cmd.Parameters.AddWithValue("@photo", txtImage.Text)
+                cmd.ExecuteNonQuery()
+            End Using
+            MessageBox.Show("Entry inserted successfully")
+            ShowGrid() ' Refresh grid after insertion
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    '----- MIRROR DATA -----
+    Private Sub txtFName_KeyUp(sender As Object, e As KeyEventArgs) Handles txtFName.KeyUp
+        txtFull.Text = txtFName.Text & " " & txtLName.Text
+    End Sub
+
+    '----- FOR HIDDEN PASSWORD -----
+    Private Sub chkShowPass_CheckedChanged(sender As Object, e As EventArgs) Handles chkShowPass.CheckedChanged
+        If chkShowPass.Checked = True Then
+            txtPass.UseSystemPasswordChar = False
+        Else
+            txtPass.UseSystemPasswordChar = True
+        End If
+    End Sub
+
+    '----- FOR IMAGE FILE PATH And IMAGE UPOAD -----
+    Private Sub btnImage_Click(sender As Object, e As EventArgs) Handles btnImage.Click
+
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            txtImage.Text = Path.GetDirectoryName(OpenFileDialog1.FileName) & "\" & Path.GetFileName(OpenFileDialog1.FileName)
+            PictureBox1.Image = Image.FromFile(txtImage.Text)
+            PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        End If
+    End Sub
+
+
+    Private Sub cmbUser_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbUser.SelectedIndexChanged
+        'Making Sql connection
+        Dim myConn As New SqlConnection("Data Source = .;Initial Catalog=TestDB;Integrated Security=SSPI;")
+        If myConn.State = ConnectionState.Closed Then
+            myConn.Open()
+        End If
+
+        '-----reset all fields -----
+        txtUser.Text = cmbUser.Text
+        txtFName.Text = ""
+        txtLName.Text = ""
+        txtAddress.Text = ""
+        txtPass.Text = ""
+        txtImage.Text = ""
+        rdbtnMale.Checked = False
+        rdbtnFemale.Checked = False
+        date_birth.ResetText()
+        PictureBox1.Image = Nothing
+
+        Try
+            myCmd = New SqlCommand("SELECT fname, lname,gender,address,password,DoB,photo From table1 WHERE username='" & cmbUser.Text & "'", myConn)
+            myReader = myCmd.ExecuteReader()
+            While myReader.Read
+                txtFName.Text = myReader.Item("fname")
+                txtLName.Text = myReader.Item("lname")
+                txtAddress.Text = myReader.Item("address")
+
+                If myReader.Item("gender") = 0 Then
+                    rdbtnFemale.Checked = True
+                Else
+                    rdbtnMale.Checked = True
+                End If
+
+                txtPass.Text = myReader.Item("password")
+                date_birth.Value = myReader.Item("DoB")
+                txtImage.Text = myReader.Item("photo")
+
+                If txtImage.Text = "" Then 'Do Nothing
+                Else
+                    PictureBox1.Image = Image.FromFile(txtImage.Text)
+                    PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+                End If
+            End While
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    ' -------- RESET BUTTOM : CLEAR ALL FIELDS And REFRESH DATA In SQL TABLE -----
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        ShowGrid()
+        '      -----reset all fields -----
+        txtUser.Text = ""
+        txtFName.Text = ""
+        txtLName.Text = ""
+        txtAddress.Text = ""
+        txtPass.Text = ""
+        txtImage.Text = ""
+        rdbtnMale.Checked = False
+        rdbtnFemale.Checked = False
+        date_birth.ResetText()
+        PictureBox1.Image = Nothing
+        cmbUser.Items.Clear()
+        cmbUser.ResetText()
+        FillComboBox()
+    End Sub
+
+    ' -------- DELETE BUTTOM : DELETE DATA RECORD FROM SQL TABLE -----
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Try
+            Dim myConn As New SqlConnection("Data Source = .;Initial Catalog=TestDB;Integrated Security=SSPI;")
+            If myConn.State = ConnectionState.Closed Then
+                myConn.Open()
+            End If
+            Dim deleteCmd As New SqlCommand("DELETE FROM table1 WHERE username=@username", myConn)
+            deleteCmd.Parameters.AddWithValue("@username", txtUser.Text)
+
+            Dim result As DialogResult = MessageBox.Show("Are you sure you want to delete this record?", "Delete Confirmation", MessageBoxButtons.OKCancel, MessageBoxIcon.Question)
+            If result = DialogResult.OK Then
+                ' Proceed with deletion 
+                deleteCmd.ExecuteNonQuery()
+                MessageBox.Show("Record deleted successfully!")
+            Else
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            cmbUser.Items.Clear()
+            FillComboBox()
+        End Try
+    End Sub
+
+    ' -------- UPDATE BUTTOM : UPDATE ENTRY In SQL TABLE -----
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Try
+            Dim myConn As New SqlConnection("Data Source = .;Initial Catalog=TestDB;Integrated Security=SSPI;")
+            If myConn.State = ConnectionState.Closed Then
+                myConn.Open()
+            End If
+            Dim genderValue As Integer
+            If rdbtnMale.Checked Then
+                genderValue = 1
+            ElseIf rdbtnFemale.Checked Then
+                genderValue = 0
+            End If
+            Dim updateCmd As New SqlCommand("UPDATE table1 SET fname='" & txtFName.Text & "', lname='" & txtLName.Text & "', gender='" & genderValue & "', DoB='" & date_birth.Text & "', password='" & txtPass.Text & "', address='" & txtAddress.Text & "', photo='" & txtImage.Text & "'WHERE username='" & cmbUser.Text & "'", myConn)
+            updateCmd.ExecuteNonQuery()
+            MessageBox.Show("Record updated successfully!")
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            cmbUser.Items.Clear()
+            FillComboBox()
+        End Try
+    End Sub
+
+End Class
