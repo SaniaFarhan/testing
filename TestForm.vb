f979199b6e16f0b129e7af50ce78b63130337d85
S Farhan <s.farhan81719@gmail.com>
2024-05-23 12:19:16 +0500

    chng text

--- TestForm.vb
+++ TestForm.vb
@@ -12,6 +12,6 @@
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MessageBox.Show("This is a branch text")
+        MessageBox.Show("holiday soon")
     End Sub
 End Class
